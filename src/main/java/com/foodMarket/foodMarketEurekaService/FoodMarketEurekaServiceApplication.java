e8c002eef073d2e93b21a79a66817c466d632b10
ChauMax123 <mpschauhan95@gmail.com>
2024-03-04 16:45:59 +0200

    Implementing eureka server for service registry

---
+++ src/main/java/com/foodMarket/foodMarketEurekaService/FoodMarketEurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.foodMarket.foodMarketEurekaService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class FoodMarketEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodMarketEurekaServiceApplication.class, args);
+	}
+
+}
